33061ffc958993b41314c1505c649ddc3b89b5cd
Thabit030 <57993677+Thabit030@users.noreply.github.com>
2020-03-15 17:38:06 +0100

    Create login.php

---
+++ login.php
@@ -0,0 +1,28 @@
+<?php
+
+$passwword = "something123";
+
+if ($passwword == true) {
+    echo "<h1> Aanmelden is gelukt!</h1> <br> Ga terug naar de pagina" . "<br>";
+}else{
+    echo "Voer aub het goede Wachtwoord in" . "<br>";
+}
+
+echo "<br>";
+
+echo "Home";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
